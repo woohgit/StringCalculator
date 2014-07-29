f665b038ee7ebb347906554c85df12bb2f145cc7
Adam Papai <wooh@wooh.hu>
2014-07-29 16:23:20 +0200

    Remove implementation

--- string_calculator.py
+++ string_calculator.py
@@ -1,5 +1,2 @@
 def add(string):
-    if "\n" in string:
-        string = string.replace("\n", ",")
-    return sum(map(int, string.split(","))) if string else 0
-
+    pass
--- test_add.py
+++ test_add.py
@@ -3,17 +3,4 @@ from string_calculator import add
 
 
 class TestStringCalculator(TestCase):
-    def test_empty_string_should_return_0(self):
-        assert add("") == 0
-
-    def test_print_numbers(self):
-        assert add("1") == 1
-        assert add("2") == 2
-
-    def test_add_numbers(self):
-        assert add("1,1") == 2
-        assert add("2,2") == 4
-
-    def test_newline_as_a_delimiter(self):
-        assert add("1\n1") == 2
-        assert add("1\n2") == 3
+    pass
